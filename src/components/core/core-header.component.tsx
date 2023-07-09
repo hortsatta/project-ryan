0d7c692673623e7a1cd7640f8403ad5091db6e3e
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-07-09 23:40:59 +0800

    Add memo

--- src/components/core/core-header.component.tsx
+++ src/components/core/core-header.component.tsx
@@ -1,10 +1,12 @@
+import { memo } from 'react';
 import { cx } from 'classix';
+
 import type { ComponentProps } from 'react';
 
-export function CoreHeader({ className }: ComponentProps<'div'>) {
+export const CoreHeader = memo(function ({ className }: ComponentProps<'div'>) {
   return (
     <header className={cx(className, 'fixed top-0 w-full h-20 z-[9999]')}>
       <nav className='mx-auto px-4 max-w-main h-full'>header</nav>
     </header>
   );
-}
+});
