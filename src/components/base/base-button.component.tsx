7c4b11a6eec94e649d57120952a31f9044cf3021
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-07-10 17:14:06 +0800

    Update button component

--- src/components/base/base-button.component.tsx
+++ src/components/base/base-button.component.tsx
@@ -14,6 +14,7 @@ export const BaseButton = memo(function ({
   className,
   variant,
   children,
+  ...moreProps
 }: Props) {
   if (variant === 'primary') {
     return (
@@ -23,6 +24,7 @@ export const BaseButton = memo(function ({
           'flex items-center h-[69px] uppercase drop-shadow-[0_12px_20px_rgba(0,132,255,0.4)] hover:brightness-110\
             hover:drop-shadow-[0_12px_20px_rgba(0,132,255,0.6)] transition-all',
         )}
+        {...moreProps}
       >
         <StaticImage
           src='../../assets/images/btn-primary-left.png'
@@ -30,7 +32,7 @@ export const BaseButton = memo(function ({
           placeholder='none'
         />
         <div className='relative px-5 h-full flex justify-center items-center bg-gradient-to-r from-primary to-accent'>
-          <div className='relative text-default text-white tracking-[3.5px] z-10'>
+          <div className='relative text-default tracking-[3.5px] z-10'>
             {children}
           </div>
           <div className='absolute top-0 left-0 w-full h-full'>
@@ -50,13 +52,14 @@ export const BaseButton = memo(function ({
   return (
     <button
       className={cx(
-        'py-3 px-6 inline-flex justify-center items-center gap-2 rounded-md border border-transparent uppercase text-default text-white\
+        'py-3 px-6 inline-flex justify-center items-center gap-2 rounded-md border border-transparent uppercase text-default\
           tracking-[3.5px] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all',
         (!variant || variant === 'solid') &&
           'bg-primary hover:bg-blue-500 dark:focus:ring-offset-gray-800',
         variant === 'ghost' && 'hover:bg-primary',
         className,
       )}
+      {...moreProps}
     >
       {children}
     </button>
