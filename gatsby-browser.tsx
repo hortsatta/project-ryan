b3e779cfd8aec777a5b20906fa7816aab0794615
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-07-15 22:25:44 +0800

    Implement page transition

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { AnimatePresence } from 'framer-motion';
 import('preline');
 
 import { CoreLayout } from './src/components/core/core-layout.component';
 
 import './src/styles/global.css';
 
-export const wrapPageElement = ({ element, props }) => {
+export function wrapPageElement({ element, props }) {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
-  return <CoreLayout {...props}>{element}</CoreLayout>;
+  return (
+    <AnimatePresence mode='wait'>
+      <CoreLayout {...props}>{element}</CoreLayout>
+    </AnimatePresence>
+  );
+}
+
+// wait until page exit animation has completed before updating scroll position
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  // transition duration from `layout.js` * 1000 to get time in ms
+  const TRANSITION_DELAY = 0.3 * 1000 * 2;
+
+  // if it's a "normal" route
+  if (location.action === 'PUSH') {
+    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY);
+  }
+
+  // if we used the browser's forwards or back button
+  else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0];
+    window.setTimeout(
+      () => window.scrollTo(...savedPosition),
+      TRANSITION_DELAY,
+    );
+  }
+
+  return false;
 };
--- src/components/base/base-scene.component.tsx
+++ src/components/base/base-scene.component.tsx
@@ -1,17 +1,45 @@
 import { cx } from 'classix';
-import { ComponentProps } from 'react';
+import { motion } from 'framer-motion';
 
-export function BaseScene({
-  className,
-  children,
-  ...moreProps
-}: ComponentProps<'div'>) {
+import type { ComponentProps } from 'react';
+
+type Props = ComponentProps<typeof motion.div> & {
+  book?: boolean;
+};
+
+const animation = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: 100,
+  },
+  transition: {
+    type: 'spring',
+    mass: 0.35,
+    stiffness: 75,
+    duration: 0.3,
+  },
+};
+
+export function BaseScene({ className, book, children, ...moreProps }: Props) {
   return (
-    <div
-      className={cx('px-4 pt-[80px] mx-auto max-w-main', className)}
+    <motion.div
+      className={cx(
+        'px-4 pt-[112px] mx-auto',
+        book ? 'book' : 'max-w-main',
+        className,
+      )}
+      {...animation}
       {...moreProps}
     >
       {children}
-    </div>
+    </motion.div>
   );
 }
